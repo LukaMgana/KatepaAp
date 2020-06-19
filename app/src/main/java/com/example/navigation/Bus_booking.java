c67b94b96211f6ad4b3083f0dce38f941f6e8368
luke mgana <lukemgana@gmail.com>
2020-06-19 19:42:59 +0300

    the first commit on this project

---
+++ app/src/main/java/com/example/navigation/Bus_booking.java
@@ -0,0 +1,14 @@
+package com.example.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bus_booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_booking);
+    }
+}
